886576035fa5bfcecbf8728e6e07a0241e63b1c9
lianliuwei <lianliuwei@gmail.com>
2012-09-17 11:51:15 +0800

    using the right scoped_refptr release.

--- osc_data_process/main.cc
+++ osc_data_process/main.cc
@@ -1,9 +1,13 @@
+#include <stdlib.h>
+#include <crtdbg.h>
+
 #include "base/threading/thread.h"
 #include "base/at_exit.h"
 #include "base/logging.h"
 #include "base/command_line.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/bind.h"
+#include "base/tracked_objects.h"
 
 #include "osc_data_process/factory.h"
 #include "osc_data_process/factory_host.h"
@@ -18,6 +22,11 @@ void CreateFactoryHost(SingleThreadTaskRunner* task) {
 }
 
 int main(int argc, char** argv) {
+  _CrtSetDbgFlag ( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+  using namespace logging;
+  tracked_objects::ThreadData::InitializeAndSetTrackingStatus(
+    tracked_objects::ThreadData::DEACTIVATED);
+
   using namespace logging;
 
   AtExitManager atexit;
@@ -26,8 +35,6 @@ int main(int argc, char** argv) {
   InitLogging(L"debug.log", LOG_TO_BOTH_FILE_AND_SYSTEM_DEBUG_LOG,
     LOCK_LOG_FILE, DELETE_OLD_LOG_FILE,
     DISABLE_DCHECK_FOR_NON_OFFICIAL_RELEASE_BUILDS);
-
-  Time::EnableHighResolutionTimer(true);
 
   MessageLoop ml(MessageLoop::TYPE_DEFAULT);
 
@@ -49,7 +56,9 @@ int main(int argc, char** argv) {
      TimeDelta::FromSeconds(4));
 
   MessageLoop::current()->Run();
-  g_host.release();
+  // no scoped_refptr.release() just leave the obj no dec ref count.
+  // g_host.release();
+  g_host = NULL;
   thread.Stop();
 
   return 0;
